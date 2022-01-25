f5c35c6f09c65a87d6e71389f6aa4e62216adb4b
lnannettik <l.nannetti.k@gmail.com>
2022-01-25 23:54:11 +0100

    finito

--- database/seeds/VacationsTableSeeder.php
+++ database/seeds/VacationsTableSeeder.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use\App\Vacation;
+// use Illuminate\Support\Str; -------------> include del pacchetto metodi 'string' di Laravel
+
+//include Faker
+use Faker\Generator as Faker;
+
+//include model
+use App\Vacation;
+
 
 class VacationsTableSeeder extends Seeder
 {
@@ -10,23 +17,28 @@ class VacationsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+
+    public function run(Faker $faker)
     {
-        // creazione di 20 vacation
-        
-        // 1. creazione model e importazione nel seeder
-
-        // 2. creazione dei dati 
-
+    
         for ($i = 0; $i < 20; $i++) {
             // creazione nuova istanza (row) di vacation
             $new_vacation = new Vacation();
 
             //popolazione delle colonne 
-            $new_vacation->name = 'Lorem ipsum';
-            $new_vacation->destination = 'Lorem ipsummmmmmmmmmmmmmm';
-            $new_vacation->night = rand(1, 13);
-            $new_vacation->price = rand(315, 3000);
+            $new_vacation->name = $faker->text(5);
+            $new_vacation->destination = $faker->text(12);
+            $new_vacation->night = $faker->numberBetween(2, 14);
+            $new_vacation->price = $faker->numberBetween(150, 8000);
+            /**
+             * $new_vacation->slug = Str::slug($new_vacation->title, '-');
+             * ---------- genera una slug con le parole del titolo spaziate da '-' ---------------
+             * ---------- ricorda l'include sopra use Illuminate\Support\Str; --------------------
+             * 
+             * $new_vacation->path_img = $faker->imageUrl(640, 480, 'category', true);
+             * ---------- genera un'immagine da placeholder.com con i parametri tra parentesi
+             * 
+             */
 
             //salvataggio della row nella tabella DB
             $new_vacation->save();
